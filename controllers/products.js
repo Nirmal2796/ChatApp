89e31157bca91437faab282a6647dc04a116aa60
Nirmal2796 <nirmalgadekar2796@gmail.com>
2023-12-22 19:59:31 +0530

    Models

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,23 @@
 const rootDir = require('../util/path');
 const path = require('path');
+
+const Product = require('../models/product');
+
 exports.getAddProduct = (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 };
 
-exports.postAddProduct = (req, res, next) => {
+exports.postAddProduct = (req, res, next) => {  
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProduct = (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    Product.fetchAll(products => {
+
+        res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    
+    });
+    
 };
